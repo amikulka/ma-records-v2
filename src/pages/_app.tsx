e9b95e25b9814554e95a171849cc528a9966feb6
Aaron Mikulka <aaronjmikulka@gmail.com>
2023-04-17 14:24:58 -0700

    adds route for album

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,31 +1,10 @@
 import { api } from "@/utils/api";
 import "@/styles/globals.css";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-} from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
 import { type AppProps } from "next/app";
-import { useRouter } from "next/router";
-import NavBarSignedOut from "@/components/navigation/navbar-signed-out";
 import NavBar from "@/components/navigation/navbar";
 
-//  List pages you want to be publicly accessible, or leave empty if
-//  every page requires authentication. Use this naming strategy:
-//   "/"              for pages/index.js
-//   "/foo"           for pages/foo/index.js
-//   "/foo/bar"       for pages/foo/bar.js
-//   "/foo/[...bar]"  for pages/foo/[...bar].js
-const publicPages: Array<string> = [];
-
 function MyApp({ Component, pageProps }: AppProps) {
-  // Get the pathname
-  const { pathname } = useRouter();
-
-  // Check if the current route matches a public page
-  const isPublicPage = publicPages.includes(pathname);
-
   // If the current route is listed as public, render it directly
   // Otherwise, use Clerk to require authentication
   return (
